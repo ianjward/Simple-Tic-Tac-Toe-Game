2dfba550814032b83c8b070c9f62d8fa0226a83a
ianjward <golfinian@gmail.com>
2019-03-18 20:04:00 -0700

    ;ukh

--- MyImageButton.java
+++ MyImageButton.java
@@ -3,7 +3,7 @@ import processing.core.*;
 
 public class MyImageButton extends GImageButton { 
    boolean isAvailable = true;
-   int takenBy = 30;
+   int takenBy = 30; //30 was choosen as the calculations for deciding victory can not contain 30
    
     public MyImageButton(PApplet theApplet, float p0, float p1, float p2, float p3, String[] fnames) {
         super(theApplet, p0, p1, p2, p3, fnames, null);
@@ -12,8 +12,6 @@ public class MyImageButton extends GImageButton {
     public void setImage(String[] fnames) {
     //======================================
     bimage = ImageManager.loadImage(winApp, fnames);
-    // There should be 3 images if not use as many as possible, 
-    // duplicating the last one if neccessary
     if(bimage.length != 3){
         PImage[] temp = new PImage[3];
         for(int i = 0; i < 3; i++)
@@ -22,6 +20,7 @@ public class MyImageButton extends GImageButton {
     }
     //======================================
     // resize images if needed
+  
     for(int i = 0; i < bimage.length; i++){
         if(bimage[i].width != width || bimage[i].height != height)
           bimage[i].resize((int)width, (int)height);                    
