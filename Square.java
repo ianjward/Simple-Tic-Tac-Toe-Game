a310ce87ceae987a45cea8e96d8049c47cf447ba
Ian Ward <golfinian@gmail.com>
2019-04-18 10:04:07 -0700

    commented

--- Square.java
+++ Square.java
@@ -1,29 +1,31 @@
 /**
   The Square class is responsible for tracking if it has been marked and by whom. It will display the appropriate picture as necessary.
 **/
-
-import g4p_controls.*;
-import processing.core.*;
+import g4p_controls.*; //needed for action listeners
+import processing.core.*; //a g4p dependency
 
 public class Square extends GImageButton { 
-   private boolean isAvailable = true;
-   private int takenBy = 30; //30 was choosen as the calculations for deciding victory can not possibly sum to 30
-   private String currentImage = "";
+   private boolean isAvailable = true; 
+   private int takenBy = 30; //30 means the square is available, 1=Human occupied 0=AI occupied 
+   private String currentImage = ""; //the image to be displayed on the square
    
-    public Square(PApplet theApplet, float p0, float p1, float p2, float p3, String[] fnames) {
-        super(theApplet, p0, p1, p2, p3, fnames, null);
-        currentImage = fnames[0];
-    }
+  public Square(PApplet theApplet, float p0, float p1, float p2, float p3, String[] fnames) {
+      super(theApplet, p0, p1, p2, p3, fnames, null);
+      currentImage = fnames[0];
+  }
     
-    protected void setTakenBy(boolean takenByUser){
-      if(takenByUser)
-        takenBy = 1;
-      else
-        takenBy = 0;
-    }
-    
-    protected void setImage(String[] fnames) {
-    //======================================
+  protected void setTakenBy(boolean takenByUser){
+    if(takenByUser)
+      takenBy = 1;
+    else
+      takenBy = 0;
+  }
+  //sets the image of a square 
+  //fnames is an array of file locations, index 0 holds file location for 
+  //the picture the button displays on click
+  //index 1 is the image location for the button on hover
+  //index 2 is the image location for the button on click and hold
+  protected void setImage(String[] fnames) {
     bimage = ImageManager.loadImage(winApp, fnames);
     currentImage = fnames[0];
     if(bimage.length != 3){
@@ -31,17 +33,7 @@ public class Square extends GImageButton {
         for(int i = 0; i < 3; i++)
           temp[i] = bimage[Math.min(i, bimage.length - 1)];
         bimage = temp;
-    }
-   
-    //======================================
-    // resize images if needed
-  
-   /* for(int i = 0; i < bimage.length; i++){
-      System.out.println(width);
-   
-        if(bimage[i].width != width || bimage[i].height != height)
-          bimage[i].resize((int)width, (int)height);                    
-     }  */
+        }
   }
    protected String getCurrentImage(){
       return currentImage;
